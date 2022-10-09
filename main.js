1f602990895b1951cea5eab5987885cf3d36fc96
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2022-10-09 16:29:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX=0
+noseY=0
+function preload(){
+catNose = loadImage('https://i.postimg.cc/J755s3XM/2498925-vector-flat-cat-heart-nose-vetor-removebg-preview.png');
+
+}
+
+function setup(){
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose x = " + results[0].pose.nose.y);
+    }
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+image(catNose, noseX - 60, noseY - 58, 120, 120);
+}
+
+function takeSnapshot(){
+    save('myFilterImage.png');
+}
+
